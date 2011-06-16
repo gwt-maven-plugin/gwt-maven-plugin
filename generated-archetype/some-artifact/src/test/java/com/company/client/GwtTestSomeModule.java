d7538ecb9fc834dd19e3615a171575fad74cfd79
Pierre-Yves Ricau <py.ricau@gmail.com>
2011-06-16 13:44:34 +0200

    added a little explanation

--- generated-archetype/some-artifact/src/test/java/com/company/client/GwtTestSomeModule.java
+++ generated-archetype/some-artifact/src/test/java/com/company/client/GwtTestSomeModule.java
@@ -10,6 +10,11 @@ import com.google.gwt.user.client.rpc.ServiceDefTarget;
  * GWT JUnit <b>integration</b> tests must extend GWTTestCase.
  * Using <code>"GwtTest*"</code> naming pattern exclude them from running with
  * surefire during the test phase.
+ * 
+ * If you run the tests using the Maven command line, you will have to 
+ * navigate with your browser to a specific url given by Maven. 
+ * See http://mojo.codehaus.org/gwt-maven-plugin/user-guide/testing.html 
+ * for details.
  */
 public class GwtTestSomeModule extends GWTTestCase {
 
