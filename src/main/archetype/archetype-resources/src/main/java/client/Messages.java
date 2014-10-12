b336cd9036e9cd22b223ce4c830ed68786b9ad7e
Thomas Broyer <t.broyer@ltgt.net>
2014-10-13 00:26:27 +0200

    Update archetype
     - remove gwt:i18n
     - use com.google.gwt:gwt BOM
     - include gwt-dev and gwt-codeserver as project dependencies
     - update JUnit
     - remove unneeded javax.validation

---
+++ src/main/archetype/archetype-resources/src/main/java/client/Messages.java
@@ -0,0 +1,10 @@
+package ${package}.client;
+
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  @DefaultMessage("Enter your name")
+  String nameField();
+
+  @DefaultMessage("Send")
+  String sendButton();
+}
