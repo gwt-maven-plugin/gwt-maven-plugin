dac1e1b899a0c49437b87ed47963e46e9ee66804
Thomas Broyer <t.broyer@ltgt.net>
2015-01-12 16:48:11 +0100

    Generate properties files from Messages interface in archetype

--- src/main/archetype/archetype-resources/src/main/java/client/Messages.java
+++ src/main/archetype/archetype-resources/src/main/java/client/Messages.java
@@ -1,5 +1,8 @@
 package ${package}.client;
 
+import com.google.gwt.i18n.client.LocalizableResource.Generate;
+
+@Generate(format = "com.google.gwt.i18n.server.PropertyCatalogFactory")
 public interface Messages extends com.google.gwt.i18n.client.Messages {
   
   @DefaultMessage("Enter your name")
