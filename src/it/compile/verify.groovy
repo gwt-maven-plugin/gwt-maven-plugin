3385e8aea361a0f7b0feaef9325874c3dfd717fd
Piotr Wilkin <piotr.wilkin@syndatis.com>
2016-02-03 12:01:32 +0100

    remove noaggressiveOptimizations

--- src/it/compile/verify.groovy
+++ src/it/compile/verify.groovy
@@ -41,7 +41,6 @@ assert content.contains( '-failOnError' );
 assert content.contains( '-optimize 1' );
 assert content.contains( '-Dgwt.persistentunitcache=true' );
 assert content.contains( '-XclosureCompiler' );
-assert content.contains( '-XnoaggressiveOptimizations' );
 assert content.contains( '-XcompilerMetrics' );
 assert content.contains( '-XfragmentCount 2' );
 assert content.contains( '-XnoclusterFunctions' );
