dc048d40cf2cccde89c48aaa626d42d063d70628
Ignacio Baca Moreno-Torres <ignacio@bacamt.com>
2020-05-14 21:03:51 +0200

    Update travis configuration

--- ci/deploy-snapshot.sh
+++ ci/deploy-snapshot.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_REPO_SLUG" == "gwt-maven-plugin/gwt-maven-plugin" ] && \
-   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "openjdk8" ] && \
    [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
    [ "$TRAVIS_BRANCH" == "master" ]; then
 
